24bf40a3a60654e5b3b6b250a394ebd38f732ebb
Ben Lind <mail@benlind.com>
2016-02-09 23:01:09 -0500

    Basic index

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Ben Lind</title>
+    </head>
+    <body>
+        This is the personal site of Ben Lind.
+    </body>
+</html>
