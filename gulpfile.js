9706576d5c74b137015cbf5bba8125026586c332
Ben Lind <mail@benlind.com>
2016-03-03 00:06:36 -0500

    Set up gulp, bower, bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,102 @@
-var gulp = require('gulp');
-var $    = require('gulp-load-plugins')();
+/**********************************************
+ * CONFIG
+ */
 
-var sassPaths = [
-  'bower_components/foundation-sites/scss',
-  'bower_components/motion-ui/src'
-];
+var gulp   = require('gulp'), 
+    sass   = require('gulp-sass') 
+    notify = require('gulp-notify') 
+    bower  = require('gulp-bower');
 
-gulp.task('sass', function() {
-  return gulp.src('scss/app.scss')
-    .pipe($.sass({
-      includePaths: sassPaths
-    })
-      .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
-    }))
-    .pipe(gulp.dest('css'));
+var config = {
+  source: './src/',
+  dest: './dist/',
+  bowerDir: './bower_components/' ,
+};
+
+var bootstrapSass = {
+  in: config.bowerDir + 'bootstrap-sass-official/'
+};
+
+var fontAwesome = {
+  inScss: config.bowerDir + '/fontawesome/scss',
+  inFonts: config.bowerDir + '/fontawesome/fonts/**.*',
+};
+
+var fonts = {
+  in: [
+    config.source + 'fonts/*.*',
+    bootstrapSass.in + 'assets/fonts/**/*',
+    fontAwesome.inFonts,
+  ],
+  out: config.dest + 'fonts/'
+};
+
+var scss = {
+  in: [
+    config.source + 'scss/*',
+  ],
+  out:   config.dest + 'css/',
+  watch: config.source + 'scss/*',
+  sassOpts: {
+    outputStyle: 'compressed',
+    precison: 3,
+    errLogToConsole: true,
+    includePaths: [
+      config.source + 'scss',
+      bootstrapSass.in + 'assets/stylesheets',
+      fontAwesome.inScss,
+    ]
+  }
+};
+
+var js = {
+  in: [
+    config.source + 'js/*',
+  ],
+  out: config.dest + 'js/',
+  inBootstrap: bootstrapSass.in + 'assets/javascripts/**/*',
+  outBootstrap: config.dest + 'js/bootstrap',
+};
+
+
+
+/**********************************************
+ * TASKS
+ */
+
+gulp.task('bower', function() { 
+  return bower()
+    .pipe(gulp.dest(config.bowerDir));
 });
 
-gulp.task('default', ['sass'], function() {
-  gulp.watch(['scss/**/*.scss'], ['sass']);
+// Move fonts
+gulp.task('fonts', function () {
+  return gulp
+    .src(fonts.in)
+    .pipe(gulp.dest(fonts.out));
+});
+
+// Compile scss
+gulp.task('sass', ['fonts'], function () {
+  return gulp.src(scss.in)
+    .pipe(sass(scss.sassOpts)
+      .on('error', notify.onError(function (error) {
+        return "Error: " + error.message;
+      })))
+    .pipe(gulp.dest(scss.out));
+});
+
+// Move js
+gulp.task('js', function () {
+  gulp
+    .src(js.in)
+    .pipe(gulp.dest(js.out));
+  return gulp
+    .src(js.inBootstrap)
+    .pipe(gulp.dest(js.outBootstrap));
+});
+
+// Default task
+gulp.task('default', ['bower', 'fonts', 'sass', 'js'], function () {
+   gulp.watch(scss.watch, ['sass']);
 });
--- index.php
+++ index.php
@@ -1,24 +1,62 @@
-<!doctype html>
-<html class="no-js" lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Ben Lind</title>
+  <title>Bootstrap 101 Template</title>
 
-    <link rel="stylesheet" href="css/app.css">
-  </head>
-  <body>
-    <div class="row">
-      <div class="large-12 columns">
-        <h1>Ben Lind's Personal Site</h1>
+  <!-- Bootstrap -->
+  <link href="dist/css/main.css" rel="stylesheet">
+
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+  <div class="navbar navbar-inverse">
+    <div class="container">
+      <div class="navbar-header">
+        <a href="#" class="navbar-brand">Ben Lind</a>
+        <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      </div>
+      <div class="navbar-collapse collapse" id="navbar-main">
+
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">About</a></li>
+          <li><a href="#">Work</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+
       </div>
     </div>
+  </div>
 
-    <script src="bower_components/jquery/dist/jquery.js"></script>
-    <script src="bower_components/what-input/what-input.js"></script>
-    <script src="bower_components/foundation-sites/dist/foundation.js"></script>
-    <script src="js/app.js"></script>
-  </body>
+  <div class="jumbotron">
+    <div class="container">
+      <h1>Blast Off</h1>
+      <p>
+        Launch your website into the future.
+      </p>
+    </div>
+  </div>
+
+  <div class="container">
+    <h1>My Projects</h1>
+
+    <h1>Contact Me</h1>
+  </div>
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="dist/js/bootstrap/bootstrap.min.js"></script>
+</body>
 </html>
